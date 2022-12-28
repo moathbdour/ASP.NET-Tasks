0cce3d238e0a0c9bc88f1cb8cdfeb4521a1b468e
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2022-12-29 01:05:59 +0300

    index

---
+++ Task2/task2.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="task2.aspx.cs" Inherits="Task2.task2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"rel="stylesheet"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                     <div class="container my-5 py-5">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-12 col-lg-10 col-xl-8">
+        <div class="card">
+          <div class="card-body" id="moath" runat="server">
+            
+          </div>
+          <div class="card-footer py-3 border-0" style="background-color: #f8f9fa;">
+            <div class="d-flex flex-start w-100">
+              <img class="rounded-circle shadow-1-strong me-3"
+                src="t.jpg" alt="avatar" width="40"
+                height="40" />
+              <div class="form-outline w-100">
+                <textarea runat="server" class="form-control" id="textAreaExample" rows="4"
+                  style="background: #fff;"></textarea>
+                <label class="form-label" for="textAreaExample">Message</label>
+              </div>
+            </div>
+            <div class="float-end mt-2 pt-1">
+                <asp:Button class="btn btn-primary btn-sm" ID="Button1" runat="server" Text="Post comment" OnClick="Button1_Click" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+            <section style="background-color: #eee;">
+ 
+</section>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Task2/task2.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Task2
+{
+    public partial class task2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("data source = DESKTOP-9HB31TL; database = comments ; integrated security=SSPI");
+            connection.Open();
+           
+            SqlCommand command1 = new SqlCommand("select * from comments1 order by id asc ", connection);
+            SqlDataReader reader = command1.ExecuteReader();
+            moath.InnerHtml = "";
+            while (reader.Read())
+            {
+                moath.InnerHtml += "<div class=\"d-flex flex-start align-items-center\">\r\n " +
+                    "             <img class=\"rounded-circle shadow-1-strong me-3\"\r\n  " +
+                    "              src=\"t.jpg\" alt=\"avatar\" width=\"60\"\r\n  " +
+                    "              height=\"60\" />\r\n              <div>\r\n  " +
+                    "              <h6 class=\"fw-bold text-primary mb-1\">Moath Bdour</h6>\r\n    " +
+                    "            <p class=\"text-muted small mb-0\">\r\n            " +
+                    $"      Shared publicly - {reader[2]}\r\n                </p>\r\n     " +
+                    $"         </div>\r\n            </div>\r\n\r\n            <p class=\"mt-3 mb-4 pb-2\"> {reader[1]}       " +
+                    "     </p>\r\n\r\n            <div class=\"small d-flex justify-content-start\">\r\n   " +
+                    "           <a href=\"#!\" class=\"d-flex align-items-center me-3\">\r\n          " +
+                    "      <i class=\"far fa-thumbs-up me-2\"></i>\r\n          " +
+                    "      <p class=\"mb-0\">Like</p>\r\n              </a>\r\n           " +
+                    "   <a href=\"#!\" class=\"d-flex align-items-center me-3\">\r\n     " +
+                    "           <i class=\"far fa-comment-dots me-2\"></i>\r\n            " +
+                    "    <p class=\"mb-0\">Comment</p>\r\n              </a>\r\n         " +
+                    "     <a href=\"#!\" class=\"d-flex align-items-center me-3\">\r\n     " +
+                    "           <i class=\"fas fa-share me-2\"></i>\r\n               " +
+                    " <p class=\"mb-0\">Share</p>\r\n              </a>\r\n            </div>";
+            }
+            connection.Close();
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection =new SqlConnection("data source = DESKTOP-9HB31TL; database = comments ; integrated security=SSPI");
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into comments1 values (@moath ,@dates)" , connection);
+            command.Parameters.AddWithValue("@moath", textAreaExample.Value.ToString());
+            command.Parameters.AddWithValue("@dates", DateTime.Now.ToString());
+            command.ExecuteNonQuery();
+            connection.Close();
+            connection.Open();
+            SqlCommand command1 = new SqlCommand("select * from comments1 order by id asc ", connection);
+            SqlDataReader reader= command1.ExecuteReader();
+            moath.InnerHtml = "";
+            while(reader.Read())
+            {
+                moath.InnerHtml += "<div class=\"d-flex flex-start align-items-center\">\r\n " +
+                    "             <img class=\"rounded-circle shadow-1-strong me-3\"\r\n  " +
+                    "              src=\"t.jpg\" alt=\"avatar\" width=\"60\"\r\n  " +
+                    "              height=\"60\" />\r\n              <div>\r\n  " +
+                    "              <h6 class=\"fw-bold text-primary mb-1\">Moath Bdour</h6>\r\n    " +
+                    "            <p class=\"text-muted small mb-0\">\r\n            " +
+                    $"      Shared publicly - {reader[2]}\r\n                </p>\r\n     " +
+                    $"         </div>\r\n            </div>\r\n\r\n            <p class=\"mt-3 mb-4 pb-2\"> {reader[1]}       " +
+                    "     </p>\r\n\r\n            <div class=\"small d-flex justify-content-start\">\r\n   " +
+                    "           <a href=\"#!\" class=\"d-flex align-items-center me-3\">\r\n          " +
+                    "      <i class=\"far fa-thumbs-up me-2\"></i>\r\n          " +
+                    "      <p class=\"mb-0\">Like</p>\r\n              </a>\r\n           " +
+                    "   <a href=\"#!\" class=\"d-flex align-items-center me-3\">\r\n     " +
+                    "           <i class=\"far fa-comment-dots me-2\"></i>\r\n            " +
+                    "    <p class=\"mb-0\">Comment</p>\r\n              </a>\r\n         " +
+                    "     <a href=\"#!\" class=\"d-flex align-items-center me-3\">\r\n     " +
+                    "           <i class=\"fas fa-share me-2\"></i>\r\n               " +
+                    " <p class=\"mb-0\">Share</p>\r\n              </a>\r\n            </div>";
+            }
+            connection.Close();
+
+            textAreaExample.Value="";
+        }
+    }
+}
